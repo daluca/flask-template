2a05ff2713a17c30454f6ac8636b9c04c124a3e4
Lucas Slebos <lucas@slebos.nz>
2021-09-15 21:59:25 +1200

    lint testing

--- src/config.py
+++ src/config.py
@@ -1,6 +1,11 @@
 """Flask configuration."""
 
 
+
+
+hello = "This is an unnecessarily long string for the purpose of testing out my linting with tox which invokes flake8 along with all its plugins"
+
+
 class Config:
     """Base configuration accross all environments."""
 
