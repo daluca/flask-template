cd78c1e70dd18ee2ccc8d686155f77f8ac69b666
Lucas Slebos <lucas@slebos.nz>
2021-09-16 00:14:27 +1200

    python version testing

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -5,3 +5,12 @@ def test_flask():
     """Get better testing doc string."""
     x = str
     assert x == str
+
+
+def test_python_version():
+    """Bad doc string."""
+    x = 6
+    y = f"{x = }"
+
+    assert y == "x = 6"
+    assert y == str
