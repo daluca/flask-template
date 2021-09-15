a40ebc2cb35a9636eecdc9d921eea1c5dc58a7b6
Lucas Slebos <lucas@slebos.nz>
2021-09-15 22:10:27 +1200

    adjust testing

--- src/config.py
+++ src/config.py
@@ -5,7 +5,6 @@
 
 hello = "This is an unnecessarily long string for the purpose of testing out my linting with tox which invokes flake8 along with all its plugins"
 
-
 class Config:
     """Base configuration accross all environments."""
 
